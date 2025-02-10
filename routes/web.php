5dc79d535603f61b778a572957cbf7601f29d848
maftunaShukhratovna <brightstarmaftuna@gmail.com>
2025-02-10 16:20:39 +0500

    home ui

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('auth/login');
+});
+
+Route::get('/register', function () {
+    return view('auth/register');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/takequiz', function () {
+    return view('quiz/takequiz');
 });
